e4ee48504a760b236fe0f96c769dfa0cba8f6de3
Federico Ibba <federicoibba@easy-network.it>
2020-05-05 11:12:18 +0200

    changed name to SignUpPage

---
+++ app/containers/SignUpPage/constants.js
@@ -0,0 +1,11 @@
+/*
+ *
+ * SignUp constants
+ *
+ */
+
+export const SIGN_UP_REQUEST = 'app/SignUp/SIGN_UP_REQUEST';
+
+export const SIGN_UP_SUCCESS = 'app/SignUp/SIGN_UP_SUCCESS';
+
+export const SIGN_UP_ERROR = 'app/SignUp/SIGN_UP_ERROR';
