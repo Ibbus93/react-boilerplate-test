72b3382568e1992ce9b8c590be14ee0f76e1c106
Federico Ibba <federicoibba@easy-network.it>
2020-05-06 10:33:19 +0200

    added mocks and snapshots

---
+++ app/containers/SignUpPage/tests/mocks/index.js
@@ -0,0 +1,27 @@
+export const user = {
+  name: 'Name',
+  surname: 'Surname',
+  username: 'username',
+  email: 'test@email.com',
+  password: 'password',
+};
+
+export const response = {
+  data: {
+    name: 'Gino',
+    surname: 'Bartali',
+    email: 'gino@bartali.it',
+    username: 'bartali',
+  },
+};
+
+export const requestData = { user };
+
+export const error = 'fake error';
+
+export const defaultState = {
+  loading: false,
+  isError: false,
+  response: null,
+  error: null,
+};
